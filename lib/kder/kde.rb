5818b6d696de8bbba6eeed5f771b6aeaa2b7fc85
Ryan Taylor <ryanmt@byu.net>
2013-06-05 13:27:59 -0400

    clean ups for kde, added benchmarking utility

--- lib/kder/kde.rb
+++ lib/kder/kde.rb
@@ -7,30 +7,40 @@ class Kder
   MeshCount = 2e3
   MinimumThresholdValue = 1e-2
   MinimumStepSize = 1e-3
+  DifferenceThreshold = 1e-2
   class << self
     ## 
     # :singleton-method: kde
     # Accepts a single member array plus optional additional information
     # Returns a two member array, [x_vals,y_vals] representing the kde
-    def kde(arr, bw = nil, opts = {sigmas: Sigmas, sampling_density: MeshCount, threshold: MinimumThresholdValue})
+    def kde(arr, bw = nil, opts = {sigmas: Sigmas, sampling_density: MeshCount, threshold: MinimumThresholdValue, minimum_delta: DifferenceThreshold})
       unless bw # is nil
         bw = Bandwidth.silverman(arr)
       end
       # Initialization steps
-      min = arr.min - bw*opts[:sigmas]
-      max = arr.max + bw*opts[:sigmas]
+      range = bw*opts[:sigmas]
+      min = arr.min - range
+      max = arr.max + range
       step_size = (max-min)/(opts[:sampling_density].to_f)
       step_size = step_size < MinimumStepSize ? MinimumStepSize : step_size
       arr.sort!
+      # initialize the range variables
+      ranges = (min..max).step(step_size).to_a
+      output = [[min,0]]
+      old_intensity = 0
       # Step through the range
-      output = (min..max).step(step_size).map do |mid|
-        high_end = mid+ bw*opts[:sigmas]
-        lower_end = mid - bw*opts[:sigmas]
-        included = arr.select {|a| (lower_end..high_end).include?(a)}
-        intensity = included.map {|a| Kder::Statistics.custom_pdf(a-mid, bw) }.inject(:+)
-        intensity ||= 0
-        [mid, intensity ] unless intensity < opts[:threshold]
+      ranges[1..-1].map.with_index do |mid, i|
+        high_end = mid + range
+        lower_end = mid - range
+        selection_range = (lower_end..high_end)
+        included = arr.select {|a| selection_range.include?(a)}
+        intensity = included.map {|a| Kder::Statistics.custom_pdf(a-mid, bw) }.inject(:+) || 0
+        unless intensity < opts[:threshold] or (intensity - old_intensity).abs < opts[:minimum_delta]
+          output << [mid, intensity ] 
+          old_intensity = intensity
+        end
       end
+      output << [max,0]
       output.compact.transpose
     end
   end
--- spec/benchmark_spec.rb
+++ spec/benchmark_spec.rb
@@ -6,14 +6,21 @@ describe Kder do
     require 'ruby-prof'
     before do
       @test_arr =10_000.times.map{ rand(100)}
+      @size = 1_000
     end
     it "gives a kde for a large array" do 
       resp = Kder.kde @test_arr
       resp.size.should eq(2)
       # TODO expand the tests
     end
-    it "PROFILES a KDE for a VERY large array" do 
-      @test_arr = 10_000_000.times.map{ rand(100)}
+    it "base time for an array of #{@times}" do 
+      @test_arr = @size.times.map{ rand(100)}
+      time = Time.now
+      resp = Kder.kde @test_arr
+      puts "Time passed: #{Time.now - time}"
+    end
+    it "PROFILES a KDE for an array of #{@times}" do 
+      @test_arr = @size.times.map{ rand(100)}
       result = RubyProf.profile do 
         resp = Kder.kde @test_arr
       end
